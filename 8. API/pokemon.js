1b754f275b3d9446eba296aa51a6c07b5b0c2495
torquemadass <ghaniyo43@gmail.com>
2024-08-16 20:39:42 +0700

    feat: api pokemon

---
+++ 8. API/pokemon.js
@@ -0,0 +1,45 @@
+// const idOrName = "pikachu";
+// const url = `https://pokeapi.co/api/v2/pokemon/${idOrName}`;
+
+// // V1 (.then)
+// fetch (url)
+//     .then((response) => {
+//         if (response.ok) {
+//         return response.json();
+//         } else {
+//             throw `${response.status}: ${response.statusText}`; //or throw new Error(response.statusText)
+//         }
+//     })
+//     .then((response) => {
+//         // console.log(response);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+
+// V2 (Async Await)
+async function getPokemonStats(nameOrId) {
+    try {
+        const url = `https://pokeapi.co/api/v2/pokemon/${nameOrId}`;
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    const pokemon = await response.json();
+
+    const pokemonStats = pokemon.stats.map((status) => {
+        return {
+            name: status.stat.name,
+            value: status.base_stat
+        }
+    })
+    
+    console.log(pokemonStats);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getPokemonStats("pikachu");
