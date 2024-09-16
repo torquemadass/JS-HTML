a60d990dda91978ba2fda4e61e1645566d1ee229
torquemadass <ghaniyo43@gmail.com>
2024-09-16 18:51:07 +0700

    feat: weekend quiz but still haven't finished

---
+++ 3. Backend/1. npm/weekend-task-npm/src/controllers/bookControllers.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+const seed = 100;
+
+class BookController {
+    getBook = async (req, res) => {
+        try {
+            const { limit = 10 } = req.query;
+            const { data: { data : books }} = await axios.get(`https://fakerapi.it/api/v2/books?_quantity=${limit}&_seed=${seed}`);
+
+            res.send(books);
+        } catch (error) {
+            res.send(error);
+        }
+    }
+
+    findBook = async (req, res) => {
+        try {
+            res.setHeader("Publisher", "JavaScript Developer Class");
+
+            const { id } = req.params;
+            const { data : { data : books} } = await axios.get(`https://fakerapi.it/api/v2/books?_quantity=${limit}&_seed=${seed}`);
+            const bookFound = books.at(-1);
+
+            if(!bookFound) throw new Error("Book Not Found!");
+
+            Object.defineProperty(bookFound, "Publisher", {value:"JavaScript Developer Class"});
+
+            res.send(bookFound);
+
+        } catch (error) {
+            res.send(error);
+        }
+    }
+}
+
+export const bookController = new bookController();
---
+++ 3. Backend/1. npm/weekend-task-npm/src/routers/index.routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { bookController } from "../controllers/bookControllers.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send("JavaScript Developer Class");
+})
+
+router.get("/books", bookController.getBook);
+router.get("/books/:id", bookController.findBook);
+
+export default router
